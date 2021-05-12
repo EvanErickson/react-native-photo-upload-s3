6ff3f8623e22d7a238f5eb20ce08f3a14bf16c53
Evan Erickson <evanserickson@gmail.com>
2021-05-12 13:13:15 -0700

    got uploads working for storage.put if it is a text.txt

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
+import { withAuthenticator } from 'aws-amplify-react-native'
+import Amplify, { Storage } from 'aws-amplify'
+import config from './src/aws-exports'
+// import awsconfig from './aws-exports';
+// Might need to switch line 7 to awsconfig 
+Amplify.configure(config)
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function App() {
+
+  Storage.put('test.txt', 'File content', {
+    progressCallback(progress) {
+        console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+  },
+});
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -19,3 +34,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App)
